c39992ccdb46a18edd570dac98687f8ddcf4fc01
Dylan Harness <dharness@uwo.ca>
2016-01-01 20:54:07 -0500

    added the resolve and user initial

--- app/config/router.js
+++ app/config/router.js
@@ -18,9 +18,27 @@ var router = function($routeProvider) {
                 }
             }
         })
-        .when('/blogs', {
+        .when('/users/:username/routes/:routename/blogs/:blogname', {
             templateUrl: 'views/blog/blog-view.html',
             controller: 'BlogController',
+            resolve: {
+                currentUser: function($route, DS, User) {
+                    // Pull out the data from the route
+                    var username = $route.current.params.username;
+                    var routename = $route.current.params.routename;
+                    var blogname = $route.current.params.blogname;
+
+                    // Attempt to load the user from the cache
+                    var currentUser = User.get(username);
+                    // if the user isn't in the cache, get it from the server
+                    if (_.isUndefined(currentUser)) {
+                        currentUser = User.find(username).then(function(currentUser) {
+                            return currentUser;
+                        });
+                    }
+                    return currentUser;
+                }
+            },
             controllerAs: 'blogCtrl'
         })
         .when('/', {
